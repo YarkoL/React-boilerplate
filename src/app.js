d693b525105f65f7ff00e8a3502b88d8c68e2293
YarkoL <yarko.dev@gmail.com>
2018-02-14 20:37:59 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+import React from 'react';  
+import ReactDOM from 'react-dom';
+
+const App = () => (
+	<h1>Hello There!</h1>
+)
+
+const destination = document.getElementById("app");
+
+ReactDOM.render( <App />, destination );
+
